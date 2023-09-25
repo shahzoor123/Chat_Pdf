190d4dcf14d7e1ea2b4992505752e157bc218dd6
shahzor khan <shahzoorkhan321@gmail.com>
2023-09-25 14:28:16 +0500

    chat pdf in streamlit

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from dotenv import load_dotenv 
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text    
+    
+
+
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFS", page_icon=":books:")
+
+    st.header("Chat with multiples PDFS :books:")
+    st.text_input("Ask a question about your documents:")
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        
+        pdf_docs  = st.file_uploader("Uploads your PDFS here and click on Process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+            
+                # get df text 
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+                
+                # get the text chunks
+
+                #create vector store
+
+
+if __name__ == '__main__':
+    
+    main()
