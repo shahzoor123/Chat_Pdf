79d0bafec4388f57e51dc0a968298c08d76afcbe
shahzor khan <shahzoorkhan321@gmail.com>
2023-09-25 14:53:07 +0500

    chat pdf in streamlit

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 # from dotenv import load_dotenv 
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text    
+# def get_pdf_text(pdf_docs):
+#     text = ""
+#     for pdf in pdf_docs:
+#         pdf_reader = PdfReader(pdf)
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
+#     return text    
     
 
 
@@ -28,10 +28,11 @@ def main():
         pdf_docs  = st.file_uploader("Uploads your PDFS here and click on Process", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
+                pass
             
                 # get df text 
-                raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                # raw_text = get_pdf_text(pdf_docs)
+                # st.write(raw_text)
                 
                 # get the text chunks
 
